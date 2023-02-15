f2d122bffcb5f8f68f59ec4bb09ce253464c4468
jcazetAtAltos <117783848+jcazetAtAltos@users.noreply.github.com>
2023-02-15 23:03:47 +0000

    Update paths

--- analyses/mapping/runMap.sh
+++ analyses/mapping/runMap.sh
@@ -2,5 +2,5 @@
 
 while read acc; do
 	echo "$acc" 1>>log.txt 2>>err.txt
-	./hgBulkRnaMap_mod.sh "$acc" ../resources/trimAdapt/TruSeq3-SE.fa 1>>log.txt 2>>err.txt
+	./hgBulkRnaMap_mod.sh "$acc" TruSeq3-SE.fa 1>>log.txt 2>>err.txt
 done<prefixList.txt
